33cd5814f4ebe7107e60e98a3b00cfecf961acdb
Thomas Venturini <thomas@venturini.codes>
2024-06-15 21:08:07 +0200

    update test url

--- 021_k6_performance_tests/browser-test-1.js
+++ 021_k6_performance_tests/browser-test-1.js
@@ -20,7 +20,7 @@ export default async function () {
   const page = browser.newPage()
 
   try {
-    await page.goto('https://thomasventurini.com')
+    await page.goto('https://test.k6.io')
     page.screenshot({ path: 'screenshots/screenshot.png' })
   } finally {
     page.close()
